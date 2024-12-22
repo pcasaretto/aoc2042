135115ef5158d5356920b1df8579f81971d97676
pcasaretto <paulo.casaretto@shopify.com>
2024-12-22 16:13:45 -0300

    day 1 part 1

---
+++ day1/src/aoc2024/day1/core.clj
@@ -0,0 +1,37 @@
+(ns aoc2024.day1.core
+  (:require [clojure.string :as str]))
+
+(defn parse-input
+  "Parse input string into two vectors of numbers"
+  [input]
+  (if (str/blank? input)
+    [[] []]
+    (let [lines (str/split-lines input)
+          parse-line (fn [line]
+                       (let [[left right] (str/split line #"\s+")]
+                         [(parse-long left) (parse-long right)]))]
+      (reduce (fn [[lefts rights] line]
+                (let [[l r] (parse-line line)]
+                  [(conj lefts l) (conj rights r)]))
+              [[] []]
+              lines))))
+
+(defn calculate-distance
+  "Calculate total distance between sorted pairs"
+  [left-nums right-nums]
+  (->> (map (fn [l r] (Math/abs (- l r)))
+            (sort left-nums)
+            (sort right-nums))
+       (reduce + 0)))
+
+(defn part1
+  "Solve part 1 of the challenge"
+  [input]
+  (let [[lefts rights] (parse-input input)]
+    (calculate-distance lefts rights)))
+
+(defn part2
+  "Solve part 2 of the challenge"
+  [input]
+  ;; TODO: Implement solution
+  nil)
---
+++ day1/test/aoc2024/day1/core_test.clj
@@ -0,0 +1,65 @@
+(ns aoc2024.day1.core-test
+  (:require [clojure.test :refer :all]
+            [aoc2024.day1.core :as sut]))
+
+(def example-input
+  "3   4
+4   3
+2   5
+1   3
+3   9
+3   3")
+
+(deftest parse-input-test
+  (testing "Basic input parsing"
+    (is (= [[3 4 2 1 3 3] [4 3 5 3 9 3]]
+           (sut/parse-input example-input))))
+
+  (testing "Empty input"
+    (is (= [[] []]
+           (sut/parse-input ""))))
+
+  (testing "Single line input"
+    (is (= [[1] [2]]
+           (sut/parse-input "1 2"))))
+
+  (testing "Input with varying whitespace"
+    (is (= [[1 2] [3 4]]
+           (sut/parse-input "1    3\n2\t\t4"))))
+
+  (testing "Input with negative numbers"
+    (is (= [[-1 0] [1 -2]]
+           (sut/parse-input "-1 1\n0 -2")))))
+
+(deftest calculate-distance-test
+  (testing "Basic distance calculation"
+    (is (= 11
+           (sut/calculate-distance [3 4 2 1 3 3] [4 3 5 3 9 3]))))
+
+  (testing "Empty lists"
+    (is (= 0
+           (sut/calculate-distance [] []))))
+
+  (testing "Single element lists"
+    (is (= 5
+           (sut/calculate-distance [1] [6]))))
+
+  (testing "Lists with negative numbers"
+    (is (= 2
+           (sut/calculate-distance [-2 0] [0 -4]))))
+
+  (testing "Lists with same numbers in different order"
+    (is (= 0
+           (sut/calculate-distance [1 2 3] [3 1 2]))))
+
+  (testing "Lists with repeated numbers"
+    (is (= 0
+           (sut/calculate-distance [1 1 1] [1 1 1])))))
+
+(deftest part1-test
+  (testing "Example case"
+    (is (= 11 (sut/part1 example-input)))))
+
+(deftest part2-test
+  (testing "Part 2"
+    (is (= nil (sut/part2 "")))))

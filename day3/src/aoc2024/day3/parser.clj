5be3e010ca61104c56534a5e2c278824341971a5
pcasaretto <paulo.casaretto@shopify.com>
2025-01-12 14:48:00 -0300

    add example parse test and fix implementation

--- day3/src/aoc2024/day3/parser.clj
+++ day3/src/aoc2024/day3/parser.clj
@@ -3,64 +3,33 @@
    [aoc2024.day3.lexer :as lexer]))
 
 
-;; (defn expect
-;;   [match modify]
-;;   (fn [lexeme acc]
-;;     (when (match lexeme)
-;;       (modify acc lexeme))))
-
-;; (defn exhaust-expectations
-;;   [lexemes expectations initial-value]
-;;   (loop [lexemes lexemes
-;;          expectations expectations
-;;          acc initial-value]
-;;     (when-let [[lexeme & lexemes] lexemes]
-;;       (if-let [[expectation & expectations] expectations]
-;;         (when-let [new-acc (expectation lexeme acc)]
-;;           (recur lexemes expectations new-acc))
-;;         ; exhausted expectations
-;;         [lexemes acc]))))
-
-;; (def function-call-expectations
-;;   )
-
-;; (defn expect-function-call
-;;   [lexemes output]
-;;   (if-let [[rest output] (exhaust-expectations lexemes [] output)]
-;;     [expect-function-call rest output]
-;;     nil))
-
 (declare expect-function-call)
 (declare expect-number)
 
 (defn expect-comma-or-right-paren
   [lexemes output acc]
-  (println "expect-comma")
   (when-let [[first & rest] lexemes]
     (cond
       (= (:type first) :comma) (expect-number rest output acc)
       (= (:type first) :rightParen) [expect-function-call rest (conj output acc)]
-      :else [expect-function-call rest output])))
+      :else [expect-function-call lexemes output])))
 
 (defn expect-number
   [lexemes output acc]
-  (println "expect-number")
   (when-let [[first & rest] lexemes]
     (if (= (:type first) :number)
       (expect-comma-or-right-paren rest output (update acc :args conj (:value first)))
-      [expect-function-call rest output])))
+      [expect-function-call lexemes output])))
 
 (defn expect-left-paren
   [lexemes output acc]
-  (println "expect-left-paren")
   (when-let [[first & rest] lexemes]
     (if (= (:type first) :leftParen)
       (expect-number rest output acc)
-      [expect-function-call rest output])))
+      [expect-function-call lexemes output])))
 
 (defn expect-function-call
   [lexemes output]
-  (println "expect-function-call")
   (when-let [[first & rest] lexemes]
     (if (= (:type first) :identifier)
       (expect-left-paren rest output {:type :function-call :name (:value first) :args []})
--- day3/test/aoc2024/day3/parser_test.clj
+++ day3/test/aoc2024/day3/parser_test.clj
@@ -1,5 +1,6 @@
 (ns aoc2024.day3.parser-test
   (:require [clojure.test :refer [deftest is testing]]
+            [aoc2024.day3.lexer :as lexer]
             [aoc2024.day3.parser :as sut]))
 
 (deftest parsing
@@ -12,3 +13,13 @@
                      {:type :comma}
                      {:type :number :value 5}
                      {:type :rightParen}])))))
+
+(def example-input "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+
+(deftest example-test
+  (testing "Parsing example input"
+    (is (= [{:type :function-call :name "xmul" :args [2 4]}
+            {:type :function-call :name "do_not_mul" :args [5 5]}
+            {:type :function-call :name "mul" :args [11 8]}
+            {:type :function-call :name "mul" :args [8 5]}]
+           (sut/parse (lexer/lex example-input))))))

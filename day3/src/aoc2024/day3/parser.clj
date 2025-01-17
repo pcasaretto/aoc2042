322f29509fa549d57ce0e938cb87fb6aa632007f
pcasaretto <paulo.casaretto@shopify.com>
2025-01-17 18:02:23 -0300

    refactor parsing

--- day3/src/aoc2024/day3/parser.clj
+++ day3/src/aoc2024/day3/parser.clj
@@ -1,51 +1,81 @@
 (ns aoc2024.day3.parser)
 
-(declare expect-function-call)
-(declare expect-number)
+(defn expect-optional
+  [expectation]
+  (fn [lexemes output] (or (expectation lexemes output) [lexemes output])))
 
-(defn expect-comma-or-right-paren
-  [lexemes output acc]
-  (when-let [[first & rest] lexemes]
-    (cond
-      (= (:type first) :comma) (expect-number rest output acc)
-      (= (:type first) :rightParen) [expect-function-call rest (conj output acc)]
-      :else [expect-function-call lexemes output])))
+; consume-all consumes lexemes that fail the expectation
+; while mutating the output with the output-transformation
+; for lexemes that pass the expectation
+(defn consume-all
+  [expectation]
+  (fn [lexemes output]
+    (loop [lexemes lexemes
+           output output]
+      (if (empty? lexemes)
+        output
+        (if-let [[remaining-lexemes new-output] (expectation lexemes output)]
+          (recur remaining-lexemes new-output)
+          (recur (rest lexemes) output))))))
 
-(defn expect-number-or-right-paren
-  [lexemes output acc]
-  (when-let [[first & rest] lexemes]
-    (cond
-      (= (:type first) :number) (expect-comma-or-right-paren rest output (update acc :args conj (:value first)))
-      (= (:type first) :rightParen) [expect-function-call rest (conj output acc)]
-      :else [expect-function-call lexemes output])))
+(defn expect-zero-or-more
+  [expectation]
+  (fn [lexemes output] (loop [lexemes lexemes
+                              output output]
+                         (if-let [[remaining-lexemes new-output] (expectation lexemes output)]
+                           (recur remaining-lexemes new-output)
+                           [lexemes output]))))
 
-(defn expect-number
-  [lexemes output acc]
-  (when-let [[first & rest] lexemes]
-    (if (= (:type first) :number)
-      (expect-comma-or-right-paren rest output (update acc :args conj (:value first)))
-      [expect-function-call lexemes output])))
+(defn pass-through-output [_ output] output)
 
-(defn expect-left-paren
-  [lexemes output acc]
-  (when-let [[first & rest] lexemes]
-    (if (= (:type first) :leftParen)
-      (expect-number-or-right-paren rest output acc)
-      [expect-function-call lexemes output])))
+(defn expect-all
+  ([expectations] (expect-all expectations pass-through-output))
+  ([expectations output-transformation]
+   (fn [lexemes output]
+     (loop [[expectation & rest] expectations
+            lexemes lexemes
+            output output]
+       (if (nil? expectation)
+         [lexemes (output-transformation lexemes output)]
+         (when-let [[lexemes-remaining transformed-output] (expectation lexemes output)]
+           (recur rest lexemes-remaining transformed-output)))))))
 
+(defn expect-type
+  ([type] (expect-type type pass-through-output))
+  ([type output-transformation]
+   (fn [lexemes output]
+     (when-let [[first & rest] lexemes]
+       (when (= (:type first) type)
+         [rest (output-transformation lexemes output)])))))
+
+(defn expect-identifier [output-transformation] (expect-type :identifier output-transformation))
+(defn expect-comma [] (expect-type :comma))
+(defn expect-number [output-transformation] (expect-type :number output-transformation))
+(defn expect-right-paren [] (expect-type :rightParen))
+(defn expect-left-paren [] (expect-type :leftParen))
+
+;a function call is almost like a regex
+; identifier leftParen ( number (comma number)*)? rightParen
 (defn expect-function-call
-  [lexemes output]
-  (when-let [[first & rest] lexemes]
-    (if (= (:type first) :identifier)
-      (expect-left-paren rest output {:type :function-call :name (:value first) :args []})
-      [expect-function-call rest output])))
+  [output-transformation]
+  (fn [lexemes output]
+    ((expect-all
+      [(expect-identifier (fn [[lexeme] output] (assoc output :name (:value lexeme))))
+       (expect-left-paren)
+       (expect-optional
+        (expect-all
+         [(expect-number (fn [[lexeme] output] (update output :args (fn [args] (conj args (:value lexeme))))))
+          (expect-zero-or-more
+           (expect-all
+            [(expect-comma)
+             (expect-number (fn [[lexeme] output] (update output :args (fn [args] (conj args (:value lexeme))))))]))]))
+       (expect-right-paren)]
+      (fn [_ result] (output-transformation output result)))
+     lexemes
+     {:type :function-call :args []})))
+
 
 (defn parse
   "Convert input lexemes into a sequence of instructions"
   [lexemes]
-  (loop [lexemes lexemes
-         output []
-         next expect-function-call]
-    (if-let [[next lexemes output] (next lexemes output)]
-      (recur lexemes output next)
-      output)))
+  ((consume-all (expect-function-call (fn [output result] (conj output result)))) lexemes []))
